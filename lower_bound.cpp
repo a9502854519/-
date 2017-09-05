63dbc8719fd20430906496d367caf9dbfe273ccd
a9502854519 <a9502854519@yahoo.com.tw>
2017-09-05 22:30:01 +0800

    二分搜尋法範例程式碼

---
+++ lower_bound.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstdio>
+//#include<algorithm>
+//#include<vector>
+
+using namespace std;
+
+int* lower_bound(int* lb, int *ub, int v){
+	int* mid; ub--; lb--;
+	while(ub - lb > 1){
+		mid = lb+(ub-lb)/2;
+		if(*mid >= v) ub = mid;
+		else lb = mid;
+	}
+	return ub;
+}
+int* upper_bound(int* lb, int* ub, int v){
+	int* mid; ub--; lb--;
+	while(ub - lb > 1){
+		mid = lb+(ub-lb)/2;
+		if(*mid > v) ub = mid;
+		else lb = mid;
+	}
+	return ub;
+}
+int main(){
+	const int N = 10;
+	int d[N] = {1,1,2,2,2,3,3,4,5,6};
+	printf("%d\n", upper_bound(d, d+N, 1) - lower_bound(d, d+N, 1));
+	
+	return 0;
+}
