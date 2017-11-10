ef3d98989fddfbc105967ba9c4e0071e7d9cfe33
a9502854519 <a9502854519@yahoo.com.tw>
2017-11-10 23:01:20 +0800

    線段樹

--- 區間查詢/線段樹/Seg_Tree.cpp
+++ 區間查詢/線段樹/Seg_Tree.cpp
@@ -1,3 +1,4 @@
+//查詢區間和的線段樹
 #define MAX_N 16
 
 int dat[2 * MAX_N - 1];
@@ -5,15 +6,15 @@ int n;//the size of the segment tree
 
 void init(int n_){
 	n = 1;
-	while(n < n_) n *= 2; //The size of the segment tree must be the power of 2
+	while(n < n_) n *= 2; //區間大小必須為2的次方
 	memset(dat, 0, sizeof(dat));
 }
 void update(int x, int v){
-	int k = x + n - 1;
+	int k = x + n - 1; //從最底部開始更新
 	dat[k] += v;
 	while(k > 0){
 		k = (k - 1) / 2;
-		dat[k] = dat[2 * k + 1] + dat[2 * k + 2];
+		dat[k] += v;
 	}
 }
 int query(int a, int b, int k, int l, int r){
